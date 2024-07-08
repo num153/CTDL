cfcd815b00522b9a8d65609abbc73701008a8812
Nam Phuong <157980092+num153@users.noreply.github.com>
2024-07-08 21:18:07 +0700

    Update linked_list.cpp

--- TuHoc_brrr/linked_list.cpp
+++ TuHoc_brrr/linked_list.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//dinh nghia cau truc node
+//=========DINH NGHIA NODE=========
 struct Node{
   int data;
   Node *next; //con tro link toi node tiep theo
@@ -19,7 +19,7 @@ Node* createNode(int x){
   p->next=NULL;
   return p;
 }
-
+//=========CAC THAO TAC=========
 void addFirst(Node *&head, int x){
   Node *p=createNode(x);
   p->next=head;
@@ -47,12 +47,65 @@ void addAfter(Node*head,int vt,int x){
     q->next=p;
   }
 }
-//duyet Node
+void delFirst(Node* &head){ //chu y truyen tham chieu
+  Node *p=head;
+  if(p != NULL){
+    head=p->next;
+    p->next=NULL;
+    delete p;
+  }
+}
+void delLast(Node *&head){
+  if(head != NULL){
+    Node *last=head;
+    Node* prev=NULL;
+    //xac dinh con tro last va prev
+    while(last->next != NULL){
+      prev = last;
+      last=last->next;
+    }
+    //xoa
+    if(prev == NULL){ //Neu node chi co 1 phan tu
+      delFirst(last);
+    }
+    else{
+    prev->next=NULL;
+    delete last;
+    }
+  }
+}
+void delNode(Node* &head,int val){
+  if(head !=NULL){
+    Node *p=head;
+    Node *prev=NULL;
+    while(p->data != val){
+      prev=p;
+      p=p->next;
+    }
+    if(prev == NULL){ //neu Node chi co 1 phan tu
+      delFirst(p);
+    }
+    else{
+      prev->next=p->next;
+      p->next=NULL;
+      delete p;  
+    }
+  }
+}
+//=========DUYET VA XUAT=========
 void xuatNode(Node *head){
   Node *danh_dau=head;
   while(danh_dau!=NULL){
     cout << danh_dau->data << "\t";
     danh_dau=danh_dau->next;
+  }
+}
+//xuat dia chi cua Node
+void xuatDiachi(Node*head){
+  Node* p=head;
+  while(p!=NULL){
+    cout << p << "\t";
+    p=p->next;
   }
 }
 //tim kiem
@@ -62,6 +115,7 @@ Node* search(Node*head,int x){
     p=p->next;
   }return p;
 }
+//=========GIAI PHONG NODE=========
 void del(Node* n){
   delete n;
   n=nullptr;
@@ -77,6 +131,13 @@ int main()
     addAfter(head,30,400);
     xuatNode(head);
     Node *p = search(head,30);
-    cout << "\nVi tri cua 30: " << p;
+    cout << "\nDia chi cua cac node: " << endl;
+    xuatDiachi(head);
+    cout << "\nDia chi cua 30: " << p;
+    cout << "\nXoa node dau: ";
+    delFirst(head);
+    delLast(head);
+    delNode(head,20);
+    xuatNode(head);
     return 0;
 }
