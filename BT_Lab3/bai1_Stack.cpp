886f5de741235d15990f648f152e41356cad38f6
Nam Phuong <157980092+num153@users.noreply.github.com>
2024-07-23 09:17:42 +0700

    Create bai1_Stack.cpp

---
+++ BT_Lab3/bai1_Stack.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+/*========DINH NGHIA NODE========*/
+struct Node {
+	int data;
+	Node* next;
+};
+struct Stack {
+	Node* head;
+};
+void init(Stack& s) {
+	s.head = NULL;
+}
+Node* createStack(int x) {
+	Node* p = new Node;
+	p->data = x;
+	p->next = NULL;
+	return p;
+}
+void freeStack(Stack&s) {
+	while (s.head != NULL) {
+		Node* temp = s.head;
+		s.head = s.head->next;
+		delete temp;
+	}
+}
+bool isEmpty(Stack s) {
+	return (s.head == NULL);
+}
+void outputStack(Stack s) {
+	Node* flag = s.head;
+	while (flag != NULL) {
+		cout << flag->data << "\t";
+		flag = flag->next;
+	}
+}
+void outputFirst(Stack s) {
+	if (!isEmpty(s)) {
+		cout << "Phan tu dau: " << s.head->data << endl;
+	}
+}
+void pushStack(Stack& s, int val) {
+	Node* p = createStack(val);
+	p->next = s.head;
+	s.head = p;
+}
+void popStack(Stack& s) {
+	Node* p = s.head;
+	if (!isEmpty(s)) {
+		s.head = p->next;
+		p->next = NULL;
+		delete p;
+	}
+}
+
+int main() {
+	Stack s1;
+	int chon;
+	init(s1);
+	cout << "STACK: ";
+	pushStack(s1, 20);
+	pushStack(s1, 10);
+	pushStack(s1, 15);
+	pushStack(s1, 40);
+	outputStack(s1);
+	do {
+		cout << "\n\t\t======MENU======\n";
+		cout << "\t1.Chi hien thi phan tu dau Stack\n";
+		cout << "\t2.Xoa phan tu trong Stack\n";
+		cout << "\t3.Hien thi phan tu\n";
+		cout << "\t0.Ket thuc\n";
+		cout << "\n\t\t======END======\n";
+		cout << "Lua chon: "; cin >> chon;
+		switch (chon) {
+		case 1:
+			outputFirst(s1);
+			break;
+		case 2:
+			popStack(s1);
+			break;
+		case 3:
+			outputStack(s1);
+			break;
+		}
+	} while (chon != 0);
+	freeStack(s1);
+	system("pause");
+	return 0;
+}
