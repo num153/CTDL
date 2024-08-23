0cea3e16c8c0ae9e8eab499a42f18aedc27485b4
Nam Phuong <157980092+num153@users.noreply.github.com>
2024-08-23 22:52:00 +0700

    Update bai02_doubly_linked_list.cpp

--- A1_GK/bai02_doubly_linked_list.cpp
+++ A1_GK/bai02_doubly_linked_list.cpp
@@ -1,112 +1,197 @@
 #include <iostream>
 using namespace std;
 struct Node {
-  int data;
-  Node *next;
-  Node *prev;
+    int data;
+    Node* next;
+    Node* prev;
 };
 struct Doublell {
-  Node *head;
-  Node *tail;
+    Node* head;
+    Node* tail;
 };
-void init(Doublell &ls) {
-  ls.head = NULL;
-  ls.tail = NULL;
+void init(Doublell& ls) {
+    ls.head = NULL;
+    ls.tail = NULL;
 }
-Node *createNode(int x) {
-  Node *p = new Node;
-  p->data = x;
-  p->next = NULL;
-  p->prev = NULL;
-  return p;
+Node* createNode(int x) {
+    Node* p = new Node;
+    p->data = x;
+    p->next = NULL;
+    p->prev = NULL;
+    return p;
 }
 bool isEmpty(Doublell ls) { return ls.head == NULL; }
-void addFirst(Doublell &ls, int val) {
-  Node *p = createNode(val);
-  if (isEmpty(ls)) {
-    ls.head = p;
-    ls.tail = p;
-  } else {
-    p->next = ls.head;
-    ls.head->prev = p;
-    ls.head = p;
-  }
+void addFirst(Doublell& ls, int val) {
+    Node* p = createNode(val);
+    if (isEmpty(ls)) {
+        ls.head = p;
+        ls.tail = p;
+    }
+    else {
+        p->next = ls.head;
+        ls.head->prev = p;
+        ls.head = p;
+    }
 }
-void addLast(Doublell &ls, int val) {
-  Node *p = createNode(val);
-  if (isEmpty(ls)) {
-    ls.head = p;
-    ls.tail = p;
-  } else {
-    p->prev = ls.tail;
-    ls.tail->next = p;
-    ls.tail = p;
-  }
+void addLast(Doublell& ls, int val) {
+    Node* p = createNode(val);
+    if (isEmpty(ls)) {
+        ls.head = p;
+        ls.tail = p;
+    }
+    else {
+        p->prev = ls.tail;
+        ls.tail->next = p;
+        ls.tail = p;
+    }
 }
-void input(Doublell &ls, int &n) {
-  cout << "Num. of Node: ";
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    int val;
-    cout << "Val no." << i + 1 << ": ";
-    cin >> val;
-    addLast(ls, val);
-  }
+void input(Doublell& ls, int& n) {
+    cout << "Num. of Node: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cout << "Val no." << i + 1 << ": ";
+        cin >> val;
+        addLast(ls, val);
+    }
 }
 void output(Doublell ls) {
-  if (!isEmpty(ls)) {
-    Node *temp = ls.head;
-    while (temp != NULL) {
-      cout << temp->data << "\t";
-      temp = temp->next;
+    if (!isEmpty(ls)) {
+        Node* temp = ls.head;
+        while (temp != NULL) {
+            cout << temp->data << "\t";
+            temp = temp->next;
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
 void countNode(Doublell ls) {
-  int count = 0;
-  if (!isEmpty(ls)) {
-    Node *temp = ls.head;
-    while (temp != NULL) {
-      temp = temp->next;
-      count++;
+    int count = 0;
+    if (!isEmpty(ls)) {
+        Node* temp = ls.head;
+        while (temp != NULL) {
+            temp = temp->next;
+            count++;
+        }
+        cout << "List has " << count << " nodes\n";
     }
-    cout << "List has " << count << " nodes\n";
-  } else {
-    cout << "List has 0 node" << endl;
-  }
+    else {
+        cout << "List has 0 node" << endl;
+    }
 }
 Doublell merge(Doublell ls1, Doublell ls2) {
-  Doublell ls_merged;
-  // Empty ls1, ls2
-  if (isEmpty(ls1)) {
-    return ls2;
-  }
-  if (isEmpty(ls2)) {
-    return ls1;
-  }
-  // At least one
-  ls1.tail->next = ls2.head;
-  ls2.head->prev = ls1.tail;
-  ls_merged.head = ls1.head;
-  ls_merged.tail = ls2.tail;
-  return ls_merged;
+    Doublell ls_merged;
+    init(ls_merged);  // Only initialize the merged list
+
+    if (isEmpty(ls1)) {
+        return ls2;
+    }
+
+    if (isEmpty(ls2)) {
+        return ls1;
+    }
+
+    // If neither list is empty
+    if (!(isEmpty(ls1) && isEmpty(ls2))) {
+        ls1.tail->next = ls2.head;
+        ls2.head->prev = ls1.tail;
+        ls_merged.head = ls1.head;
+        ls_merged.tail = ls2.tail;
+
+        return ls_merged;
+    }
+}
+
+
+void free(Doublell& ls) {
+    while (ls.head != NULL) {
+        Node* temp = ls.head;
+        ls.head = ls.head->next;
+        delete temp;
+    }
+    ls.head = NULL;
+    ls.tail = NULL;
 }
 int main() {
-  Doublell ls1;
-  Doublell ls2;
-  Doublell ls3;
-  int n;
-  init(ls1);
-  init(ls2);
-  init(ls3);
-  input(ls1, n);
-  input(ls2, n);
-  output(ls1);
-  output(ls2);
-  ls3 = merge(ls1, ls2);
-  output(ls3);
-  countNode(ls3);
-  return 0;
+    Doublell ls1;
+    Doublell ls2;
+    Doublell ls3;
+    int n;
+    int choice;
+    bool flag1 = false, flag2 = false;
+    do {
+        cout << "\n\t======MENU======\t\n";
+        cout << "1. Input for list 1\n";
+        cout << "2. Input for list 2\n";
+        cout << "3. Output list 1\n";
+        cout << "4. Output list 2\n";
+        cout << "5. Count nodes in list 1\n";
+        cout << "6. Count nodes in list 2\n";
+        cout << "7. Merge 2 lists\n";
+        cout << "8. Delete lists\n";
+        cout << "0. Exit\n";
+        cout << "Your choice: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            init(ls1);
+            input(ls1, n);
+            flag1 = true;
+            break;
+        case 2:
+            init(ls2);
+            input(ls2, n);
+            flag2 = true;
+            break;
+        case 3:
+            if (flag1) {
+                output(ls1);
+            }
+            else {
+                cout << "List 1 is empty" << endl;
+            }
+            break;
+        case 4:
+            if (flag2) {
+                output(ls2);
+            }
+            else {
+                cout << "List 2 is empty" << endl;
+            }
+            break;
+        case 5:
+            if (flag1) {
+                countNode(ls1);
+            }
+            else {
+                cout << "List 1 is empty" << endl;
+            }
+            break;
+        case 6:
+            if (flag2) {
+                countNode(ls2);
+            }
+            else {
+                cout << "List 2 is empty" << endl;
+            }
+            break;
+        case 7:
+            if (flag1 || flag2) {
+                ls3 = merge(ls1, ls2);
+                output(ls3);
+            }
+            else {
+                cout << "Both lists are empty" << endl;
+            }
+            break;
+        case 8:
+            free(ls1);
+            free(ls2);
+            free(ls3);
+            flag1 = false;
+            flag2 = false;
+            break;
+        }
+    } while (choice != 0);
+    return 0;
 }
-// - Viết hàm để xoá toàn bộ phần tử danh sách liên kết kép
