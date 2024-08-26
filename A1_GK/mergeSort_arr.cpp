8ed0efa47a3ef5636b82904dae59ba80b203e048
Nam Phuong <157980092+num153@users.noreply.github.com>
2024-08-27 01:27:13 +0700

    Update mergeSort_arr.cpp

--- A1_GK/mergeSort_arr.cpp
+++ A1_GK/mergeSort_arr.cpp
@@ -113,7 +113,61 @@ void mergeSort(Node*& head) {
 	mergeSort(m);
 	head = merge(l, m);
 }
-
+//--------QUICK SORT----------
+struct Node* last_node(struct Node* head) 
+{ 
+    struct Node* temp = head; 
+    while (temp != NULL && temp->next != NULL) { 
+        temp = temp->next; 
+    } 
+    return temp; 
+} 
+struct Node* partition(struct Node* first, struct Node* last) 
+{ 
+    // Get first node of given linked list 
+    struct Node* pivot = first; 
+    struct Node* front = first; 
+    int temp = 0; 
+    while (front != NULL && front != last) { 
+        if (front->data < last->data) { 
+            pivot = first; 
+  
+            // Swapping  node values 
+            temp = first->data; 
+            first->data = front->data; 
+            front->data = temp; 
+  
+            // Visiting the next node 
+            first = first->next; 
+        } 
+  
+        // Visiting the next node 
+        front = front->next; 
+    } 
+  
+    // Change last node value to current node 
+    temp = first->data; 
+    first->data = last->data; 
+    last->data = temp; 
+    return pivot; 
+} 
+void quick_sort(struct Node* first, struct Node* last) 
+{ 
+    if (first == last) { 
+        return; 
+    } 
+    struct Node* pivot = partition(first, last); 
+  
+    if (pivot != NULL && pivot->next != NULL) { 
+        quick_sort(pivot->next, last); 
+    } 
+  
+    if (pivot != NULL && first != pivot) { 
+        quick_sort(first, pivot); 
+    } 
+} 
+// Function call 
+    quick_sort(head, last_node(head)); 
 int main() {
     int a[9] = {4, 7, 2, 9, 1, 3, 8, 99, 33};
     // int left = 0;
